ba572e9af8e624b4563ae8e20a0b1c1a51e3ec47
Toph Tucker <tophtucker@gmail.com>
2015-03-04 20:11:28 -0500

    Style refinements etc

--- public/calc/scripts.js
+++ public/calc/scripts.js
@@ -16,18 +16,18 @@ items["gold"] = [{"item":"Torch","type":"gold","price":20,"source":"http://www.g
 
 items["usd"] = [{"item":"Vaccination syringe, 5 mL","type":"usd","price":0.04,"source":"http://www.unicef.org/videoaudio/PDFs/EN-ImmSumm-2013.pdf"},
 {"item":"Polio vaccine dose","type":"usd","price":0.13,"source":"https://www.flickr.com/photos/gatesfoundation/5632251253/in/set-72157625923795751"},
-{"item":"Book for school children","type":"usd","price":0.38,"source":"http://www.aidforafrica.org/donate/"},
+{"item":"Book for schoolchild","type":"usd","price":0.38,"source":"http://www.aidforafrica.org/donate/"},
 {"item":"Meningitis vaccine dose","type":"usd","price":0.50,"source":"https://www.flickr.com/photos/gatesfoundation/5632251253/in/set-72157625923795751"},
 {"item":"Cholera vaccine dose","type":"usd","price":2.00,"source":"https://www.flickr.com/photos/gatesfoundation/5632251253/in/set-72157625923795751"},
-{"item":"Flock of ducks","type":"usd","price":20.00,"source":"http://www.heifer.org/gift-catalog/animals-nutrition/flock-of-ducks-donation.html"},
-{"item":"Honeybees","type":"usd","price":30.00,"source":"http://www.heifer.org/gift-catalog/animals-nutrition/honeybees-donation.html"},
+{"item":"Flock of ducks donation","type":"usd","price":20.00,"source":"http://www.heifer.org/gift-catalog/animals-nutrition/flock-of-ducks-donation.html"},
+// {"item":"Honeybees donation","type":"usd","price":30.00,"source":"http://www.heifer.org/gift-catalog/animals-nutrition/honeybees-donation.html"},
 {"item":"School supplies for a student for a year","type":"usd","price":50.00,"source":"http://www.aidforafrica.org/donate/"},
-{"item":"Seedlings and saplings","type":"usd","price":60.00,"source":"http://www.heifer.org/gift-catalog/sustainable-farming/gift-of-trees-donation.html"},
-{"item":"Goat","type":"usd","price":120.00,"source":"http://www.heifer.org/gift-catalog/animals-nutrition/gift-of-a-goat-donation.html"},
+{"item":"Seedlings and saplings donation","type":"usd","price":60.00,"source":"http://www.heifer.org/gift-catalog/sustainable-farming/gift-of-trees-donation.html"},
+{"item":"Goat donation","type":"usd","price":120.00,"source":"http://www.heifer.org/gift-catalog/animals-nutrition/gift-of-a-goat-donation.html"},
 {"item":"Set of irrigation pumps and training","type":"usd","price":150.00,"source":"http://www.heifer.org/gift-catalog/basic-needs/irrigation-pumps-donation.html"},
-{"item":"Water buffalo","type":"usd","price":250.00,"source":"http://www.heifer.org/gift-catalog/animals-nutrition/gift-of-a-water-buffalo-donation.html"},
+{"item":"Water buffalo donation","type":"usd","price":250.00,"source":"http://www.heifer.org/gift-catalog/animals-nutrition/gift-of-a-water-buffalo-donation.html"},
 {"item":"Send a girl to school","type":"usd","price":275.00,"source":"http://www.heifer.org/gift-catalog/empowerment/send-a-girl-to-school-donation.html"},
-{"item":"Heifer","type":"usd","price":500.00,"source":"http://www.heifer.org/gift-catalog/animals-nutrition/gift-of-a-heifer-donation.html"}];
+{"item":"Heifer donation","type":"usd","price":500.00,"source":"http://www.heifer.org/gift-catalog/animals-nutrition/gift-of-a-heifer-donation.html"}];
 
 // SLIDER
 // http://bl.ocks.org/shancarter/5979700
@@ -35,7 +35,7 @@ items["usd"] = [{"item":"Vaccination syringe, 5 mL","type":"usd","price":0.04,"s
 var width = 500;
 
 var x = d3.scale.log()
-    .domain([0.01, 1000])
+    .domain([0.1, 1000])
     .range([0, width])
     .clamp(true);
 
@@ -98,7 +98,7 @@ function initTable(type) {
   rows.append("td").classed("quantity", true).text("#");
   rows.append("td").classed("operator", true).text("×");
   rows.append("td").classed("item", true).text(function(d) { return d.item; });
-  rows.append("td").classed("price", true).text(function(d) { return d.price; });
+  // rows.append("td").classed("price", true).text(function(d) { return d.price; });
 }
 
 function renderTable(type, value) {
